3382f40e750dddf997c07011f354a53bf0d85b57
Scott Delbango <scott.delbango@gmail.com>
2020-02-23 13:04:18 -0500

    created game logic

--- memorygame/js/main.js
+++ memorygame/js/main.js
@@ -1,17 +1,22 @@
 let cards = ["Laura", "Laura", "Carl", "Carl"];
 let cardsInPlay = [];
-let cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardsInPlay[0])
-let cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardsInPlay[1])
 
-if (cardsInPlay.length === 2) {
+function checkForMatch () {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-			alert("You found a match!");
-		}
+				alert("You found a match!");
+			}
 	else {
 		alert("DID I DO THAT?");
 	}
 }
+
+function flipCard(cardId) {
+	console.log("User flipped " + cards[cardId] + ".");
+	cardsInPlay.push(cards[cardId]);
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+	}
+}
+
+flipCard(0);
+flipCard(2);
